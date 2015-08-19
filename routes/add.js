2ca1878c3f23c670d75e590742cf609adae37c23
rpenn <ryanpenn00@gmail.com>
2015-08-18 20:56:48 -0400

    initial commit

---
+++ routes/add.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+	res.render('addpage.html');
+});
+
+router.post('/submit', function(req, res) {
+  var models = require('../models/');
+
+  // STUDENT ASSIGNMENT:
+  // add definitions of the `title`, `content` and `url_name` variables here
+  var title = req.body.pageTitle;
+  var content = req.body.pageContent;
+  // var url = title.replace(/[^a-zA-Z\d\s:]/g, "");
+ 
+	var generateUrlName = function(title) {
+	  if (typeof title != "undefined" && title !== "") {
+	    // Removes all non-alphanumeric characters from title
+	    // And make spaces underscore
+	    return title.replace(/\s/ig, '_').replace(/\W/ig,'');
+	  } else {
+	    // Generates random 5 letter string
+	    return Math.random().toString(36).substring(2,7);
+	  }
+	};
+
+  var url_name = generateUrlName(title);
+  console.log(url_name);
+  var page = new models.Page({ 'title': title, 'content': content, 'url_name': url_name });
+  page.save();
+  res.redirect('/add');
+});
+
+
+module.exports = router;
